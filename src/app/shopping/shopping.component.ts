1ca0ae36322a0fec10a4fd3c494c54999174b815
Jason Leong <42172421+Jay890@users.noreply.github.com>
2021-01-28 15:11:34 +1000

    Shopping list content
    Model for ingredients in a shared directory

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
-  styleUrls: ['./shopping.component.css']
+  styleUrls: ['./shopping.component.css'],
 })
 export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 5),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
